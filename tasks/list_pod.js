2763e326b5c14371ce80de8c0aa45feb3dca5c5b
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2016-07-08 07:36:07 -0700

    format list pod

--- tasks/list_pod.js
+++ tasks/list_pod.js
@@ -2,9 +2,28 @@ const utils = require('../utils');
 const _ = require('lodash');
 
 function getPodsByEmail(pods, email) {
+  if (email === 'all') {
+    return pods;
+  }
+
   return pods.filter(pod => {
     return _.find(pod.Parameters, { ParameterKey: 'Creator', ParameterValue: email });
   });
+}
+
+function getPodUrl(podName, type) {
+  const name = podName.toLowerCase();
+  if (type === 'new') {
+    return {
+      manage: `https://${name}-manage.symphonycommerce.com`,
+      site: `https://${name}-kittydemo.symphonycommerce.com`,
+    };
+  }
+
+  return {
+    manage: `https://${name}-partner.symphonycommerce.com`,
+    site: `https://${name}-sites.symphonycommerce.com`,
+  };
 }
 
 module.exports = (req, res) => {
@@ -12,7 +31,10 @@ module.exports = (req, res) => {
   let userEmail = utils.getUserEmail(req.query.user_id);
   const overrideEmail = (req.query.text || '').trim();
   // if specifying another persons email
-  if (overrideEmail && overrideEmail.indexOf('@symphonycommerce.com') !== -1) {
+  if (overrideEmail && overrideEmail.toLowerCase() === 'all') {
+    userEmail = '';
+  }
+  else if (overrideEmail && overrideEmail.indexOf('@symphonycommerce.com') !== -1) {
     userEmail = overrideEmail;
   }
 
@@ -21,10 +43,17 @@ module.exports = (req, res) => {
       const userPods = getPodsByEmail(pods, userEmail);
       const userPodsInfo = userPods.map(pod => {
         const param = utils.cfParam2Obj(pod.Parameters);
+        const createdAt = new Date(pod.CreationTime)
+          .toISOString().replace(/T/, ' ').replace(/\..+/, '');
+        const type = /-pod/.test(pod.StackName) ? 'new' : 'old';
+        const urls = getPodUrl(param.PodName, type);
+        const branch = `${param.GitBranch} <${urls.manage}|manage> <${urls.site}|site>`;
+
         return {
           podName: param.PodName,
-          branch: param.GitBranch,
-          createdAt: pod.CreationTime,
+          type,
+          branch,
+          createdAt,
           status: pod.StackStatus,
         };
       });
