4782c1ed962b27cd28164b0fc8d7a84d0594d246
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2016-07-11 13:55:16 -0700

    new pod support expires, so no need to add that

--- tasks/create_pod_v2.js
+++ tasks/create_pod_v2.js
@@ -4,7 +4,7 @@ const podModel = require('../model/pod_creation');
 
 const DEFAULT_SPOT_PRICE = 0.075;
 const MAX_SPOT_PRICE = 0.163;
-const DEFAULT_PERSIST_DAY = 3;
+const DEFAULT_PERSIST_HOURS = 8;
 
 module.exports = (req, res) => {
 
@@ -16,13 +16,13 @@ module.exports = (req, res) => {
   const branch = text[0].replace(/\//g, '.');
   const name = (text[1] || camelCase(branch)).replace(/\./g, '');
   const price = parseFloat(text[2]) || DEFAULT_SPOT_PRICE;
-  const persistDay = text[3] ?
+  const persistHours = text[3] ?
     (
       isNaN(parseInt(text[3])) ?
-      DEFAULT_PERSIST_DAY :
+      DEFAULT_PERSIST_HOURS :
       parseInt(text[3])
     ) :
-    DEFAULT_PERSIST_DAY;
+    DEFAULT_PERSIST_HOURS;
 
   if (price > MAX_SPOT_PRICE) {
     return res.status(200).send(`Spot price should not be greater than ${MAX_SPOT_PRICE}`);
@@ -40,14 +40,12 @@ module.exports = (req, res) => {
     cause: 'slack',
     public_ip: '127.0.0.1',
     private_dns: '127.0.0.1',
-    expires: persistDay * 24,
+    expires: persistHours,
   };
 
   utils.callJenkins(qs)
     .then(() => {
       res.status(200).send('Create pod successful!');
-      podModel.insert(name, persistDay)
-        .catch(console.log);
     })
     .catch(err => {
       return res.status(200).send('Create pod failed:' + err);
